b6ff2bc467a0ed79d17121a8c5c37ba0dbc9f469
armanavasthi <aravasth@cisco.com>
2021-06-26 22:50:50 +0530

    test api with usage of feign client

--- request-service/src/main/java/com/rescue/requestservice/controller/RequestController.java
+++ request-service/src/main/java/com/rescue/requestservice/controller/RequestController.java
@@ -1,9 +1,23 @@
 package com.rescue.requestservice.controller;
 
+import com.rescue.requestservice.proxy.LocationProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RequestController {
+    @Autowired
+    LocationProxy locationProxy;
 
+    @GetMapping("/")
+    public ResponseEntity<Boolean> isWorking() {
+
+        Boolean response = locationProxy.retrieveisLocationWorking();
+        return new ResponseEntity(response, HttpStatus.OK);
+
+    }
 
 }
--- request-service/src/test/java/com/rescue/requestservice/RequestServiceApplicationTests.java
+++ request-service/src/test/java/com/rescue/requestservice/RequestServiceApplicationTests.java
@@ -3,7 +3,10 @@ package com.rescue.requestservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		properties = {"spring.cloud.config.enabled=false"}
+)
 class RequestServiceApplicationTests {
 
 	@Test
