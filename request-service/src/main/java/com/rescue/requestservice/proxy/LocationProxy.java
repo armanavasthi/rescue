c3a8c2b88d736e0f48a9c3d8f275eeaa364b116c
aravasth <aravasth@cisco.com>
2021-06-26 22:50:50 +0530

    test api with usage of feign client

---
+++ request-service/src/main/java/com/rescue/requestservice/proxy/LocationProxy.java
@@ -0,0 +1,11 @@
+package com.rescue.requestservice.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name="currency-exchange-service", url="localhost:6060")
+public interface LocationProxy {
+    @GetMapping("/")
+    public Boolean retrieveisLocationWorking();
+
+}
