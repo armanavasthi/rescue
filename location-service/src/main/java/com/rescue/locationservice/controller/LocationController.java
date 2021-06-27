316cb15c48c02cfe2ecf5c40eb4bbe05ef44c413
Arman Avasthi <avasthiarman@gmail.com>
2021-06-27 15:21:10 +0530

    Sample api communication using feign client  (#1)
    * test api with usage of feign client
    
    * removed commented unnecessary code
    
    * changing github gradle workflow action java version to 8
    
    * using test profile for pr builder
    
    Co-authored-by: aravasth <aravasth@cisco.com>

--- location-service/src/main/java/com/rescue/locationservice/controller/LocationController.java
+++ location-service/src/main/java/com/rescue/locationservice/controller/LocationController.java
@@ -1,8 +1,16 @@
 package com.rescue.locationservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LocationController {
+
+    @GetMapping("/")
+    public ResponseEntity<Boolean> isWorking() {
+        return new ResponseEntity(true, HttpStatus.OK);
+    }
 
 }
--- location-service/src/test/java/com/rescue/locationservice/LocationServiceApplicationTests.java
+++ location-service/src/test/java/com/rescue/locationservice/LocationServiceApplicationTests.java
@@ -3,7 +3,10 @@ package com.rescue.locationservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		properties = {"spring.cloud.config.enabled=false"}
+)
 class LocationServiceApplicationTests {
 
 	@Test
