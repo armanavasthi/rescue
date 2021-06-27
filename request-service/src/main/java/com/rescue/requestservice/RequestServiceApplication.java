b1ae839aea7539e764810b9835b7fb625001ac24
Arman Avasthi <avasthiarman@gmail.com>
2021-06-27 15:21:10 +0530

    Sample api communication using feign client  (#1)
    * test api with usage of feign client
    
    * removed commented unnecessary code
    
    * changing github gradle workflow action java version to 8
    
    * using test profile for pr builder
    
    Co-authored-by: aravasth <aravasth@cisco.com>

--- request-service/src/main/java/com/rescue/requestservice/RequestServiceApplication.java
+++ request-service/src/main/java/com/rescue/requestservice/RequestServiceApplication.java
@@ -2,8 +2,10 @@ package com.rescue.requestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.rescue.requestservice")
 public class RequestServiceApplication {
 
 	public static void main(String[] args) {
